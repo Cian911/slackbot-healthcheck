222d2af7aabbda612bc736f3a2ad42af63fb8f4c
cian <cian@ciangallagher.net>
2016-05-03 16:16:08 +0100

    Removed time restartints

--- app/controllers/slack_post_message_controller.rb
+++ app/controllers/slack_post_message_controller.rb
@@ -14,14 +14,14 @@ class SlackPostMessageController < ApplicationController
 
     # Check time for message
     time = Time.now.strftime("%H:%M").to_i
-
-    if time === 9
-      client.chat_postMessage( channel: '#dev-test', text: 'It\'s time to stretch! *Sitting is the new cancer*. Get up for a short walk! Go grab a coffe, tea or fuck it, grab a beer!' , as_user: true )
-    elsif time === 12
-      client.chat_postMessage( channel: '#dev-test', text: 'It\'s time to stretch! *Sitting WILL KILL YOU*. Get up and stretch you lazy maggot!' , as_user: true )
-    elsif time === 16
-      client.chat_postMessage( channel: '#dev-test', text: 'It\'s time to stretch! *Sitting is..* .You know at this staging. Get up for a short walk! Go grab a coffe, tea or fuck it, granb a beer!' , as_user: true )
-    end
+    client.chat_postMessage( channel: '#dev-test', text: 'It\'s time to stretch! *Sitting is the new cancer*. Get up for a short walk! Go grab a coffe, tea or fuck it, grab a beer!' , as_user: true )
+    # if time === 9
+    #   client.chat_postMessage( channel: '#dev-test', text: 'It\'s time to stretch! *Sitting is the new cancer*. Get up for a short walk! Go grab a coffe, tea or fuck it, grab a beer!' , as_user: true )
+    # elsif time === 12
+    #   client.chat_postMessage( channel: '#dev-test', text: 'It\'s time to stretch! *Sitting WILL KILL YOU*. Get up and stretch you lazy maggot!' , as_user: true )
+    # elsif time === 16
+    #   client.chat_postMessage( channel: '#dev-test', text: 'It\'s time to stretch! *Sitting is..* .You know at this staging. Get up for a short walk! Go grab a coffe, tea or fuck it, granb a beer!' , as_user: true )
+    # end
   end
 
 end
